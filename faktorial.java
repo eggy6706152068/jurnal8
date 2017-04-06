993e34685f5c4dec13b4d2970d874b33c86205a1
Eggy Prayogia A <eprayogia@live.com>
2017-04-06 09:29:11 +0700

    Add files via upload

---
+++ faktorial.java
@@ -0,0 +1,60 @@
+import java.io.*;
+class bintang
+{
+public static void main (String [] args) throws Exception
+{
+DataInputStream input=new DataInputStream(System.in);
+int m,x,y,n;
+ 
+
+System.out.print("inputkan jumlah baris : ");
+int br=Integer.parseInt(input.readLine());
+for (int i=1;i<=br;i++)
+{
+if (i <=4) {
+for (int j=1;j<=br;j++)
+{
+if (j<=4)
+{
+m=i+j;
+if (5<=m)
+System.out.print ("*");
+else
+System.out.print (" ");
+}
+else
+{
+x=j-3;
+if(x<=i)
+System.out.print ("*");
+else
+System.out.print (" ");
+}
+}
+}
+else
+{
+for (int j=1;j<=br;j++)
+{
+if (j<=4)
+{
+y=i-3;
+if (y<=j)
+System.out.print ("*");
+else
+System.out.print (" ");
+}
+else
+{
+x=j-3; y=i-3; n=x+y;
+if(n<=5)
+System.out.print ("*");
+else
+System.out.print (" ");
+}
+}
+}
+System.out.println ();
+}
+}
+}
